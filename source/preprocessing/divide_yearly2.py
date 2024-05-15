89495f5361dfc40f1b3b09286baf57e9999ba11d
JinPyoK <111625932+JinPyoK@users.noreply.github.com>
2024-05-15 16:27:48 +0900

    modify comment

--- source/preprocessing/divide_yearly2.py
+++ source/preprocessing/divide_yearly2.py
@@ -2,16 +2,16 @@ import pandas as pd
 
 data = pd.read_csv('./data/country_scaled2/all_scaled.csv')
 
-# divide the data into groups by country
+# divide the data into groups by year
 def divideYearly():
   year = data['Year'].unique()
   # ['1991', '1992', ... '2020']
 
   for y in year:
-    # Select rows by country
+    # Select rows by year
     yearly_data = data[data['Year'] == y]
 
-    # Write csv file by country
+    # Write csv file by year
     yearly_data.to_csv('./data/yearly_scaled2/{}.csv'.format(y), index=False)
 
 divideYearly()
